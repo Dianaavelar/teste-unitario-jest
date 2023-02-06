9332c87381bf67aff4e2e2afd76bc71c7052fdf4
Dianaavelar <104920494+Dianaavelar@users.noreply.github.com>
2023-02-06 15:44:51 -0300

    Primeiro commit

---
+++ tests/calcs.test.js
@@ -0,0 +1,23 @@
+let {sum, diff, product, div} = require('./../src/calcs');
+const calculators = require('./../src/calcs');
+
+
+test('A soma de 1 e 2 deve ser igual a 3', () => {
+    expect(sum(1, 2)).toBe(3);
+    
+});
+
+test('A diferença de 10 e 2 deve ser igual a 8', ( ) => {
+    expect(diff(10, 2)).toBe(8);
+
+});
+
+test('A multiplicaçao de 4 e 2 deve ser igual a 8', ( ) => {
+    expect(product(4, 2)).toBe(7);
+
+});
+
+test('A divisao de 8 e 2 deve ser igual a 4', ( ) => {
+    expect(div(8, 2)).toBe(4);
+
+});
